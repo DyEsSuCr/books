6ce0cf96ffa11b1e76a250be1461ecb97fa7eddb
DylanSuarez <2000sajo@gmail.com>
2023-02-18 11:59:20 -0500

    validate post author

--- server/routes/authors.routes.js
+++ server/routes/authors.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { validateCreate } from '../validators/genres.js'
 import {
   delAuthors,
   getAuthor,
@@ -11,7 +12,7 @@ const router = Router()
 
 router.get('/authors/:id', getAuthor)
 router.get('/authors/', getAuthors)
-router.post('/authors/', postAuthors)
+router.post('/authors/', validateCreate, postAuthors)
 router.put('/authors/:id', putAuthors)
 router.delete('/authors/:id', delAuthors)
 
