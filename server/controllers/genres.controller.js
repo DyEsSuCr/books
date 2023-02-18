77b93d2b8c3cd75efc504c22c2186becbb500a8e
DylanSuarez <2000sajo@gmail.com>
2023-02-17 22:52:06 -0500

    trycatch

--- server/controllers/genres.controller.js
+++ server/controllers/genres.controller.js
@@ -3,65 +3,94 @@ import { Genres } from '../models/Genres.js'
 export const getGenre = async (req, res) => {
   const { id } = req.params
 
-  const genre = await Genres.findOne({
-    where: {
-      id,
-    },
-    attributes: ['id', 'name', 'createdAt', 'updatedAt'],
-  })
+  try {
+    const genre = await Genres.findOne({
+      where: {
+        id,
+      },
+      attributes: ['id', 'name', 'createdAt', 'updatedAt'],
+    })
 
-  res.status(200).json(genre)
+    res.status(200).json(genre)
+  } catch (err) {
+    res.status(404).json({ error: err })
+  }
 }
 
 export const getGenres = async (req, res) => {
-  const genres = await Genres.findAll({
-    where: {
-      state: true,
-    },
-    attributes: ['id', 'name', 'createdAt', 'updatedAt'],
-  })
+  try {
+    const genres = await Genres.findAll({
+      where: {
+        state: true,
+      },
+      attributes: ['id', 'name', 'createdAt', 'updatedAt'],
+    })
 
-  if (genres.length <= 0) return res.status(200).json({ messaje: 'No hay generos' })
+    if (genres.length <= 0) return res.status(200).json({ messaje: 'No hay generos' })
 
-  res.status(200).json(genres)
+    res.status(200).json(genres)
+  } catch (err) {
+    res.status(404).json({ error: err })
+  }
 }
 
 export const postGenres = async (req, res) => {
   const { name } = req.body
 
-  const newGenre = await Genres.create({
-    name,
-  })
+  try {
+    const genre = await Genres.create({
+      name,
+    })
 
-  res.status(201).json(newGenre)
+    const id = genre.id
+
+    const newGenre = await Genres.findAll({
+      where: {
+        id,
+      },
+      attributes: ['id', 'name', 'createdAt', 'updatedAt'],
+    })
+
+    res.status(201).json(newGenre)
+  } catch (err) {
+    res.status(404).json({ error: err })
+  }
 }
 
 export const putGenres = async (req, res) => {
   const { id } = req.params
   const { name } = req.body
 
-  const genreUpdate = await Genres.findOne({
-    where: {
-      id,
-    },
-    attributes: ['id', 'name', 'createdAt', 'updatedAt'],
-  })
+  try {
+    const genreUpdate = await Genres.findOne({
+      where: {
+        id,
+      },
+      attributes: ['id', 'name', 'createdAt', 'updatedAt'],
+    })
 
-  genreUpdate.name = name
+    genreUpdate.name = name
 
-  await genreUpdate.save()
+    await genreUpdate.save()
 
-  res.status(200).json(genreUpdate)
+    res.status(200).json(genreUpdate)
+  } catch (err) {
+    res.status(404).json({ error: err })
+  }
 }
 
 export const delGenres = async (req, res) => {
   const { id } = req.params
 
-  const genre = await Genres.findByPk(id)
+  try {
+    const genre = await Genres.findByPk(id)
 
-  genre.state = false
+    genre.state = false
 
-  await genre.save()
+    await genre.save()
 
-  res.status(200).json({ messaje: 'Eliminado' })
+    res.status(200).json({ messaje: 'Eliminado' })
+  } catch (err) {
+    res.status(404).json({ error: err })
+  }
 }
