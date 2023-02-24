92e559f0bd0109278815544795d0f10eaab2b141
DylanSuarez <2000sajo@gmail.com>
2023-02-23 23:36:05 -0500

    onetomany

--- server/models/Asociations.js
+++ server/models/Asociations.js
@@ -2,8 +2,22 @@ import { Authors } from './Authors.js'
 import { Books } from './Books.js'
 import { Genres } from './Genres.js'
 
+// Block: Asocitation (ManyToMany) Authors
+
+Books.belongsToMany(Authors, {
+  through: 'authors_books',
+  timestamps: false,
+})
+
 Authors.belongsToMany(Books, {
   through: 'authors_books',
+  timestamps: false,
+})
+
+// Block: Asocitation (ManyToMany) Genres
+
+Books.belongsToMany(Genres, {
+  through: 'genres_books',
   timestamps: false,
 })
 
@@ -12,12 +26,46 @@ Genres.belongsToMany(Books, {
   timestamps: false,
 })
 
-Books.belongsToMany(Authors, {
-  through: 'authors_books',
-  timestamps: false,
+// Block: Asocitation (OneToMany) Authors
+
+Books.hasMany(Authors, {
+  foreignKey: 'bookId',
+  sourceKey: 'id',
 })
 
-Books.belongsToMany(Genres, {
-  through: 'genres_books',
-  timestamps: false,
+Authors.belongsTo(Books, {
+  foreignKey: 'bookId',
+  sourceKey: 'id',
+})
+
+Books.belongsTo(Authors, {
+  foreignKey: 'authorId',
+  sourceKey: 'id',
+})
+
+Authors.hasMany(Books, {
+  foreignKey: 'authorId',
+  sourceKey: 'id',
+})
+
+// Block: Asocitation (OneToMany) Genres
+
+Books.hasMany(Genres, {
+  foreignKey: 'bookId',
+  sourceKey: 'id',
+})
+
+Genres.belongsTo(Books, {
+  foreignKey: 'bookId',
+  sourceKey: 'id',
+})
+
+Books.belongsTo(Genres, {
+  foreignKey: 'genreId',
+  sourceKey: 'id',
+})
+
+Genres.hasMany(Books, {
+  foreignKey: 'genreId',
+  sourceKey: 'id',
 })
