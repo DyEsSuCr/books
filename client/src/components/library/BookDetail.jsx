0fe5656f70981c82febd948bebc4902977d2d091
DylanSuarez <2000sajo@gmail.com>
2023-02-24 18:37:54 -0500

    bookDetail

--- client/src/components/library/BookDetail.jsx
+++ client/src/components/library/BookDetail.jsx
@@ -1,7 +1,7 @@
 import { useLoaderData, Link } from 'react-router-dom'
 
 export function BookDetail() {
-  const { book } = useLoaderData()
+  const { book, genres } = useLoaderData()
 
   return (
     <>
@@ -9,19 +9,24 @@ export function BookDetail() {
         <Link>{book.tittle}</Link>
         <p>{book.subtitle}</p>
         <img className="max-w-lg" src={book.image} alt={book.tittle} />
-        {book.genres.map((genre) => (
-          <span className="gap-2" key={genre.id}>
-            {genre.name}
-          </span>
-        ))}
+        {genres.map((genre) => {
+          return (
+            <div key={genre.id}>
+              <span>{genre.name}</span>
+            </div>
+          )
+        })}
       </div>
     </>
   )
 }
 
 export async function loaderBookDetail({ params }) {
-  const res = await fetch(`http://localhost:3000/api/books/${params.id}`)
-  const book = await res.json()
+  const resBook = await fetch(`http://localhost:3000/api/books/${params.id}`)
+  const book = await resBook.json()
 
-  return { book }
+  const resGenres = await fetch(`http://localhost:3000/api/books/${book.id}/genres`)
+  const genres = await resGenres.json()
+
+  return { book, genres }
 }
